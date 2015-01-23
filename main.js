17a85ed2ccd7696590096c0a5544e63dce3273a8
Paweł Łaski <laski@wit.edu.pl>
2015-01-23 01:22:51 +0100

    remove index.html and rename index2.html

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 "use strict";
-var toastGroupTemplate = document.querySelector('#toastGroup');
-toastGroupTemplate.showToast = function() {
-    document.querySelector('#toast').show();
-//     var a = 1;
-}
+
